e9f3052ad10c0ac10a06973d46322739fbb766e4
SAMUEL KOBINA GYASI <gyasisamuelkobina22@gmail.com>
2022-07-07 17:45:43 +0100

    music player app

---
+++ script.js
@@ -0,0 +1,118 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+
+const songs =['Elle', 'Ommema','Deserve Glory'];//song titles
+
+let songIndex=2;
+
+
+loadSong(songs[songIndex]);
+
+
+function loadSong(song) 
+{
+    title.innerHTML = song;
+    audio.src=`musics/${song}.mp3`;
+    cover.src=`images/${song}.jpg`;
+}
+
+
+function playSong()
+{
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+
+    audio.play();
+
+}
+
+
+//pauseSong Function
+function pauseSong()
+{
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+
+    audio.pause();
+
+}
+
+//previous song Function
+function prevSong()
+{
+  songIndex--;
+  if(songIndex<0)
+  {
+    songIndex=songs.length-1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+//nextsong Function
+function nextSong()
+{
+  songIndex++;
+  if(songIndex>songs.length - 1)
+  {
+    songIndex=0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+
+function updateProgress(e)
+{
+const { duration,currentTime}=e.srcElement;
+progressPercent=(currentTime/duration)*100;
+progress.style.width=`${progressPercent}%`;
+}
+
+function setProgress(e)
+{
+    const width=this.clientWidth;
+    const clickX=e.offsetX;
+
+    const duration=audio.duration;
+
+    audio.currentTime=(clickX/width)*duration;
+
+
+    // 
+}
+
+//playButton EventListeners
+playBtn.addEventListener("click", ()=>{
+    const isPlaying=musicContainer.classList.contains("play");
+
+    if(isPlaying)
+     {
+        pauseSong();
+     }
+     else
+     {
+        playSong();
+     }
+});
+
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+audio.addEventListener("timeupdate",updateProgress);
+progressContainer.addEventListener("click", setProgress);
